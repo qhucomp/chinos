4f587de3d0444d63413b20843f420f79f5490d86
qhuos <qhuoscomp@protonmail.com>
2021-03-31 22:34:25 +0800

    remove user/*

--- src/main.c
+++ src/main.c
@@ -7,15 +7,15 @@
 #include <stdio.h>
 #include <sysctl.h>
 #include <uarths.h>
-#include "user_syscall.h"
 
 int main(void) {
-  //chino os init
+  // chino os init
   sysctl_pll_set_freq(SYSCTL_PLL0, 800000000UL);
   plic_init();
   sysctl_enable_irq();
-
-  syscall(0, 1, 2, 3, 4, 5, 6);
-  while (1);
+  sleep(5);
+  printf("hello world\n");
+  while (1)
+    ;
   return 0;
 }
