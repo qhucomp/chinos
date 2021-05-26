74f8c996eddc940d57fb533d8fccd12b0c95034c
qhuos <qhuoscomp@protonmail.com>
2021-05-27 01:13:14 +0800

    update

--- kernel/user.c
+++ kernel/user.c
@@ -22,14 +22,14 @@ task_struct *user_thread(const char *name) {
     // printk("read ok!\n");
     vfs_read(p,elf,p->file_size);
     // vfs_read(p,elf,p->file_size);
-    // printk("elf ptr:%p p->file_size:%d\n",elf,p->file_size);
+    printk("elf ptr:%p p->file_size:%d\n",elf,p->file_size);
     size_t size = 0;
     uint64_t offset;
     // 构建进程在内存中的镜像
     memset(user_space,0,USER_HEAP_SIZE);
     // memset((void *)0x80200000,0,0x400000);
     size += copy_section(elf,user_space,".text",&offset);
-    // printk("offset:%p\n",offset);
+    printk("offset:%p\n",offset);
     task->task_reg.x6 = task->epc = (uintptr_t)user_space + offset;
     task->task_reg.x1 = (uintptr_t)user_exit;
     size += copy_section(elf,user_space,".rodata",NULL);
@@ -39,7 +39,7 @@ task_struct *user_thread(const char *name) {
     task->task_reg.x11 = task->sp = (uintptr_t)user_space + USER_HEAP_SIZE;
     task->brk_base = 0;
     add_task(task);
-    // printk("task epc:%p\n",task->epc);
+    printk("task epc:%p\n",task->epc);
     return task;
 }
 
