003b70970ded29b34b8652ab9a5b32dde3c79f34
qhuos <qhuoscomp@protonmail.com>
2021-05-29 18:10:45 +0800

    update

--- kernel/shell.c
+++ kernel/shell.c
@@ -65,8 +65,9 @@ int user_shell(void) {
 
     ECALL(SYS_user_task,run_list[22],0,0,0,0,0);
     ECALL(SYS_wait4,-1,&status,0,0,0,0);
-    // ECALL(SYS_user_task,run_list[12],0,0,0,0,0);
-    // ECALL(SYS_wait4,-1,&status,0,0,0,0);
+
+    ECALL(SYS_user_task,run_list[12],0,0,0,0,0);
+    ECALL(SYS_wait4,-1,&status,0,0,0,0);
         printk("\nend test\n");
     while(1);
     return 1;
